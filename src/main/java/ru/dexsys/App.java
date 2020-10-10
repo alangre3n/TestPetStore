2aa198c772a49b8ce281d6988cba248af78f3def
machettie <55490804+machettie@users.noreply.github.com>
2020-10-10 22:48:31 +0400

    удалил лишнее

--- src/main/java/ru/dexsys/App.java
+++ src/main/java/ru/dexsys/App.java
@@ -2,13 +2,6 @@ package ru.dexsys;
 
 public class App {
     public static void main(String[] args) {
-          /*  PetRequest.addPet(1488, "Lucky", "Test tag - 1");
-            PetRequest.findPetById(1488);
-            PetRequest.updatePet(1488, "Steven", "New test tag - 2!", PetStatus.sold);
-            PetRequest.findPetById(1488);
-            PetRequest.deletePet(1488);
-            PetRequest.findPetByStatus(PetStatus.sold);
-            */
 
         PetRequest.addPet();
 
